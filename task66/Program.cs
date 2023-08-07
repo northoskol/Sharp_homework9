f51d7584c6e0943eba630539c69658148465cf6e
Dmitry <northoskol@gmail.com>
2023-08-08 01:42:29 +0300

    end task 66

--- task66/Program.cs
+++ task66/Program.cs
@@ -2,4 +2,17 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+int SumNaturalNumbers(int firstNumber, int SecondNumber)
+{
+    if (firstNumber > SecondNumber) return 0;
+    return firstNumber + SumNaturalNumbers(firstNumber + 1, SecondNumber);
+}
 
+Console.Write("Введите число M: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int sum = SumNaturalNumbers(firstNumber, secondNumber);
+Console.WriteLine("Сумма натуральных элементов в диапазоне от " + firstNumber + " до " + secondNumber + ": " + sum);
