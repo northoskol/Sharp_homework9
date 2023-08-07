6bbaeecd2aadb7808e8e44c1f17640e58540a508
Dmitry <northoskol@gmail.com>
2023-08-08 01:53:29 +0300

    end task68

--- task68/Program.cs
+++ task68/Program.cs
@@ -3,3 +3,18 @@
 // m = 3, n = 2 -> A(m,n) = 29
 
 
+int Akkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0 && m > 0) return Akkerman(m - 1, 1);
+    else return (Akkerman(m - 1, Akkerman(m, n - 1)));
+}
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int result = Akkerman(m, n);
+Console.WriteLine($"Результат вычисления функции Аккермана: " + result);
